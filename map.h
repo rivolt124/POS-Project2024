c21e8bc93673f74f563e5198a590b262b92234b8
Robinqos <155308194+Robinqos@users.noreply.github.com>
2024-12-30 10:12:09 +0100

    Update map.h

--- map.h
+++ map.h
@@ -1,10 +1,7 @@
-// Funkcia na inicializáciu mapy
 void createMap(char** map, int width, int height);
 
-// Funkcia na vykreslenie mapy
 void drawMap(char** map, int width, int height);
 
-// Funkcia na generovanie náhodných objektov na mapu
 void generate(char** map, int width, int height, char object, int count);
 
 
