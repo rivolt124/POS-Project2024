86dfbd42a237e4f6db486ab44bbee3551f6af3f8
Your Name <cedzorobo@gmail.com>
2025-01-02 20:29:11 +0100

    VERZIA 1.1 Pridane metody : - increaseSize                  - changeDirection                  - checkCollision

--- Server/gameLogic.c
+++ Server/gameLogic.c
@@ -1,5 +1,7 @@
 #include "gameLogic.h"
 
+#include <stdlib.h>
+
 
 void showSnake(SnakeAtributes* snake, Map* map) {
     if (!snake->isLive) return;
@@ -14,4 +16,22 @@ bool isApple(int x, int y, Map map) {
 
 bool isPassable(int x, int y, Map map) {
     return map.data[x][y] == ' ' || isApple(x, y, map); // if its empty ' ' and its not an apple, then its passable
+}
+
+void increaseSize(SnakeAtributes* snake) {
+    snake->size += 1;
+}
+
+void changeDirection(SnakeAtributes* snake, int newHeading) {
+    if (abs(newHeading - snake->heading) != 180) {          //only if its not going backwards
+        snake->heading = newHeading;
+    }
+}
+
+void checkCollision(SnakeAtributes* snake, Map* map) {
+    if (map->data[snake->x][snake->y] == '#' ||
+        map->data[snake->x][snake->y] == '~' ||
+        map->data[snake->x][snake->y] == '|') {
+        snake->isLive = 0;
+        }
 }
