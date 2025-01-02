5c3946b59af7493cf9ac48d26bb10ba7893f7d02
Your Name <cedzorobo@gmail.com>
2025-01-02 20:13:47 +0100

    VERZIA 1.0 doplnene- metody showSnake         - isApple(este treba dorobit asi v mape to generovanie jablk... to bude to iste ako sa generuju prekazky)         - isPassable

--- Server/gameLogic.c
+++ Server/gameLogic.c
@@ -1,1 +1,17 @@
+#include "gameLogic.h"
 
+
+void showSnake(SnakeAtributes* snake, Map* map) {
+    if (!snake->isLive) return;
+
+    // Show player name(character)
+    map->data[snake->x][snake->y] = snake->name;
+}
+
+bool isApple(int x, int y, Map map) {
+    return map.data[x][y] == 'o'; // Jablko bude ako o
+}
+
+bool isPassable(int x, int y, Map map) {
+    return map.data[x][y] == ' ' || isApple(x, y, map); // if its empty ' ' and its not an apple, then its passable
+}
